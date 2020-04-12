2160a074864552de61dd4ea821f334cadf994c76
Hermes Netto <hebn.94@gmail.com>
2020-04-12 18:52:55 -0300

    Implement the authentication on spotify api

---
+++ src/custom-hooks/useSpotifyToken.ts
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+
+interface UseSpotifyTokenActions {
+  readToken: () => string | null;
+  saveToken: (token: string) => void;
+}
+
+const useSpotifyToken = (): [string | null, UseSpotifyTokenActions] => {
+  const [token, setToken] = useState<string>('');
+  const key = 'gts-spotify-token';
+
+  useEffect(() => {
+    const value = readToken();
+    if (value) {
+      setToken(value);
+    }
+  }, []);
+
+  const readToken = (): string | null => {
+    return localStorage.getItem(key);
+  };
+
+  const saveToken = (token: string): void => {
+    localStorage.setItem(key, token);
+    setToken(token);
+  };
+
+  return [token, { readToken, saveToken }];
+};
+
+export default useSpotifyToken;
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,48 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import Brand from '../components/Brand';
 import Button from '../components/Button';
+import useSpotifyToken from '../custom-hooks/useSpotifyToken';
+
+const spotifyAuthEndpoint = 'https://accounts.spotify.com/authorize';
+const clientId = 'd9d505e880594b7ca174cf7feeb525ea';
+const redirectUri = 'http://localhost:3000/';
+const scopes = ['user-top-read'];
 
 const HomeScreen: React.FC = () => {
+  const [token, { readToken, saveToken }] = useSpotifyToken();
+  const history = useHistory();
+
+  useEffect(() => {
+    const hash = window.location.hash;
+
+    if (hash) {
+      saveToken(
+        hash
+          .replace('#', '')
+          .split('&')[0]
+          .split('=')[1]
+      );
+
+      history.push('/setup-game');
+    }
+  }, []);
+
+  const spotifySignIn = () => {
+    if (!token && !readToken()) {
+      window.location.href = `${spotifyAuthEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+        '%20'
+      )}&response_type=token&show_dialog=true`;
+    } else {
+      history.push('/setup-game');
+    }
+  };
+
   return (
     <>
       <Brand />
-      <Button as={Link} to="/setup-game" size="lg" block>
+      <Button size="lg" onClick={spotifySignIn} block>
         Start Game
       </Button>
     </>
