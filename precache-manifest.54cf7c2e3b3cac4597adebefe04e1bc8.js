826a009f68b4780ca4c83603674834973af3ff80
Hermes Netto <hebn.94@gmail.com>
2020-04-14 22:22:01 -0300

    Updates

---
+++ precache-manifest.54cf7c2e3b3cac4597adebefe04e1bc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a80d8ffcf9f9aeaf9a09e788c3764e7d",
+    "url": "/guess-the-song/index.html"
+  },
+  {
+    "revision": "e5fd932cc9860c7b286c",
+    "url": "/guess-the-song/static/css/main.032d73cf.chunk.css"
+  },
+  {
+    "revision": "ea0d9e632e110f10ad37",
+    "url": "/guess-the-song/static/js/2.178be644.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/guess-the-song/static/js/2.178be644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5fd932cc9860c7b286c",
+    "url": "/guess-the-song/static/js/main.65c47b7e.chunk.js"
+  },
+  {
+    "revision": "8b5f4a6b2e0dce7357fc",
+    "url": "/guess-the-song/static/js/runtime-main.28af7aba.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c06ab1b1176f9d18b34540d7821c0492.js"
+  "/guess-the-song/precache-manifest.54cf7c2e3b3cac4597adebefe04e1bc8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/guess-the-song/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
