b9dee2246e72434b5f94189571cbc62eb53d73b8
Hermes Netto <hebn.94@gmail.com>
2020-04-14 22:17:52 -0300

    Updates

---
+++ precache-manifest.c06ab1b1176f9d18b34540d7821c0492.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eb91a59f6221e35fd028b250231eef8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e04d1bde14e9a10e6692",
+    "url": "/static/css/main.032d73cf.chunk.css"
+  },
+  {
+    "revision": "65d43d1b846d7577cb3f",
+    "url": "/static/js/2.2da08812.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2da08812.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e04d1bde14e9a10e6692",
+    "url": "/static/js/main.4131a603.chunk.js"
+  },
+  {
+    "revision": "75a19600db59ee385d7c",
+    "url": "/static/js/runtime-main.44ecc1ca.js"
+  }
+]);
